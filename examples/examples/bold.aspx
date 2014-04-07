3b810f8887ba8574f6ab9ec9d6d12d25b9a8d3e7
Robert Plummer <robertleeplummerjr@gmail.com>
2014-04-07 09:29:50 -0400

    Initial commit, not yet working
    Just before reaching out to the guys at Phalanger.

---
+++ examples/examples/bold.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="bold.aspx.cs" Inherits="examples.bold" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server"></form>
+</body>
+</html>
---
+++ examples/examples/bold.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Query.Dynamic;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PHP.Core;
+using WikiLingo;
+
+namespace examples
+{
+    public partial class bold : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var text = "test";
+        }
+    }
+}
