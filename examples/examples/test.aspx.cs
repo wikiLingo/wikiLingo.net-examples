625854e1b1fb4bfd7855eb5ed321106c5f2e67de
Robert Plummer <robertleeplummerjr@gmail.com>
2014-05-29 16:58:21 -0400

    Modified to now use Testify
    To run through all the unit tests that are in wikiLingo.  It fails for
    now, but should give some very strait forward ways to make Phalanger
    much better.

---
+++ examples/examples/test.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using PHP;
+using PHP.Core;
+using PHP.Library;
+using WikiLingo;
+
+namespace examples
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var c = ScriptContext.CurrentContext;
+            var globals = c.Globals;
+            ApplicationContext.Default.AssemblyLoader.Load(typeof(WikiLingo.Parser).Assembly, null);
+
+
+            c.Include("Testify\\Testify.php", true);
+            c.Include("test.php", true);
+
+            var t = "";
+        }
+    }
+}
---
+++ examples/examples/test.php
@@ -0,0 +1,26 @@
+﻿<?php
+
+$tf = new \Testify\Testify("wikiLingo test suite");
+
+//Test Expressions
+$tf->test("WikiLingo Expressions", function($tf) {
+	(new WikiLingo\Test\TypeNamespace("Expression"))->run($tf);
+});
+
+//Test Expression Error Recovery
+$tf->test("WikiLingo Expression Error Recovery", function($tf) {
+	(new WikiLingo\Test\TypeNamespace("ExpressionErrorRecovery"))->run($tf);
+});
+
+//Test Plugins
+$tf->test("WikiLingo Plugins", function($tf) {
+	(new WikiLingo\Test\TypeNamespace("Plugin"))->run($tf);
+});
+
+//Test WYSIWYGWikiLingo Syntax Generator
+$tf->test("WYSIWYGWikiLingo Syntax Generator", function($tf) {
+	(new WYSIWYGWikiLingo\Test\TypeNamespace("SyntaxGenerator"))->run($tf);
+});
+
+ob_start();
+$tf();
